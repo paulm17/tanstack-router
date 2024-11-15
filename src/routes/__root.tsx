b9af733556ce4c7253c34f24c465063df72f8c47
Paul Moss <paulmoss06@gmail.com>
2024-11-15 19:19:19 +0000

    build: another commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { RaikouProvider } from '@raikou/system';
+import { emotionTransform, RaikouEmotionProvider } from "@raikou/emotion";
+
 
 import '@raikou/system/styles.css';
 import '@stylefusion/react/styles.css';
@@ -12,8 +14,10 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <RaikouProvider>
-        <Outlet />
+      <RaikouProvider stylesTransform={emotionTransform}>
+        <RaikouEmotionProvider>
+          <Outlet />
+        </RaikouEmotionProvider>
       </RaikouProvider>
       <TanStackRouterDevtools position="bottom-right" />
     </>
